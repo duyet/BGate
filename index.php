c00df28efd371169ede9e85dcde22567453e55a1
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-05-13 13:40:50 +0700

    Edit index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php 
-require('public/index.php');
+
+//chdir(dirname(__DIR__));
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
