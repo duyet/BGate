92c109039ef0144bbc944dd9ed43f2bbd937d826
LvDuit <lvduit08@gmail.com>
2015-05-19 19:59:34 +0700

    Reupdate bgate.js

--- bgate.js
+++ bgate.js
@@ -1,99 +1,47 @@
 /**
- * CDNPAL NGINAD Project
- *
- * @link http://www.nginad.com
- * @copyright Copyright (c) 2013-2015 CDNPAL Ltd. All Rights Reserved
- * @license GPLv3
+ * Tracking and display ads for BGate system.
+ * 
+ * Date: 19/05/2015
+ * Author: ISLab
  */
 
-var adserver_domain = 'server.nginad.com';
-var script_name = 'nginad.js';
+var adserverDomain = 'localhost';
 var delivery_path = '/delivery/impress';
 
-var quality_scoring_pixels = [                   
-	// QS SERVICE 1
-	// "http://myqualityscore1.com/server/pixel.htm?uid=user_id", 	
-	// QS SERVICE 2
-	// "http://myqualityscore2.com/server/pixel.htm?uid=user_id",				
-	// QS SERVICE 3
-	// "http://myqualityscore3.com/server/pixel.htm?uid=user_id",		
-];
+if (typeof ISLab_AdsiFrame_Opts === "undefined") {
+	var ISLab_AdsiFrame_Opts = null;
+}
 
-var cookie_matching_pixels = [                   
-	// ZENOVIA EXCHANGE
-	// "http://sync.nj.zenoviaexchange.com/usersync2/partner_id", 		
-	// TURN
-	// "http://ad.turn.com/server/pixel.htm?fpid=13&r=12345", 	
-	// MEDIA MATH
-	// "http://pixel.mathtag.com/sync/js?sync=auto",				
-	// DSTILLERY
-	// "http://idpix.media6degrees.com/orbserv/hbpix?pixId=1",		
-	// CHANGO
-	// "http://lj.d.chango.com/m/lj?r=12345",	
-	// RFI HUB
-	// "http://p.rfihub.com/cm?in=1&pub=1",					
-	// APPNEXUS
-	// "http://ib.adnxs.com/getuid?http://mydomain.com/merge?pid=12&3pid=$UID",	
-	// RTB BIDDER
-	// "http://match.rtbidder.net/match?p=31&ord=12345",			
-	// SITE SCOUT
-	// "http://pixel.sitescout.com/dmp/pixelSync?network=partner_id",	
-	// CASALE MEDIA
-	// "http://ip.casalemedia.com/usermatch?s=178636&cb=http%3A%2F%2Fmydomain.com%2Fmerge%3Fpid%3D18%263pid%3D",	
-	// IPONWEB:
-	// "http://x.bidswitch.net/sync?ssp=fmx",
-	// TRADE DESK
-	// "http://data.adsrvr.org/track/cmf/generic?ttd_pid=partner_id",
-	// RUBICON PROJECT
-	// "http://pixel.rubiconproject.com/tap.php?v=other&nid=partner_id&put={user_token}&expires={days}",
-	// AUDIENCE SCIENCE
-	// "http://pix04.revsci.net/D08734/a3/0/3/0.302?matchId=100&PV=0"
-];
-                           
-function createTrackingPixel(url) {
-	
+if (typeof ISLab_placement_id === "undefined") {
+	var ISLab_placement_id = null;
+}
+
+var qualityScoringPixels = [];
+var cookieMatchingPixels = [];
+function initPixelTracking(url) {
 	(new Image()).src = url;
-
 }
 
 function fireCookieMatchingPixels() {
-	
-	for (i in cookie_matching_pixels) {
-		createTrackingPixel(cookie_matching_pixels[i]);
+	for (i in cookieMatchingPixels) {
+		initPixelTracking(cookieMatchingPixels[i]);
 	}
-
 }
 
 function fireQSPixels() {
-	
-	for (i in quality_scoring_pixels) {
-		createTrackingPixel(quality_scoring_pixels[i]);
+	for (i in qualityScoringPixels) {
+		initPixelTracking(qualityScoringPixels[i]);
 	}
-
 }
 
-if (typeof NGIN_AdsiFrame_Opts === "undefined") {
-	
-	var NGIN_AdsiFrame_Opts = null;
-	
-}
-
-if (typeof NGIN_placement_id === "undefined") {
-	
-	var NGIN_placement_id = null;
-
-}
-
-var NGIN_AdsiFrame=(function() {
+var script_name = 'bgate.js';
+var InitLvDuitIFrame = (function() {
 	
   function isInIframe() {
-	 
 	  return self !== top;
-  
   }
   
   function getScriptTag() {
-	  
 	  if('currentScript' in document) {
 		  return document.currentScript;
 	  }
@@ -118,18 +66,17 @@ var NGIN_AdsiFrame=(function() {
   }
   
   function getQueryString() {
-	  
-    var myScript=getScriptTag();
+
+    var myScript = getScriptTag();
     var rg = new RegExp(script_name, 'i');
     if(myScript.src.search(rg) >= 0) {
     	return myScript.src.replace(/^[^\?]+\??/,'');
-    } else {
-    	return false;
     }
+
+    return false;
   }
   
   function getQueryStringArg(qs, key, default_) {
-	  
     default_ = default_||'';
     var query_obj = {};
    
@@ -143,10 +90,9 @@ var NGIN_AdsiFrame=(function() {
   }
   
   function getSiteURL() {
-	  
     var site_loc='';
     
-    try{
+    try {
     	if(isInIframe() && document.referrer) {
     		site_loc = document.referrer.replace(/^\s+|\s+$/g,'');
     	} else {
@@ -159,7 +105,6 @@ var NGIN_AdsiFrame=(function() {
   }
   
   function getRefSiteURL() {
-	  
     var ref='';
     if(!isInIframe()) {
     	ref=document.referrer.replace(/^\s+|\s+$|["']/g,'');
@@ -168,14 +113,11 @@ var NGIN_AdsiFrame=(function() {
   }
   
   function getOD() {
-	  
 	  return parseUri(document.location.href.replace(/["']/g,'')).host;
-  
   }
 
   function getNGINZoneId(zoneid) {
-	  
-    var id = 'NGIN_' + zoneid;
+    var id = 'ISLab_' + zoneid;
     
     var obj = document.getElementById(id);
     
@@ -187,7 +129,7 @@ var NGIN_AdsiFrame=(function() {
     
     while(obj) {
       
-    	id = 'NGIN_' + zoneid + '_' + i;
+    	id = 'ISLab_' + zoneid + '_' + i;
     	obj = document.getElementById(id);
     	i++;
     }
@@ -255,7 +197,7 @@ var NGIN_AdsiFrame=(function() {
     return rect;
   }
   
-function getNGINViewport() {
+function getISLabViewport() {
 	    
 	  var viewport = {x:0,y:0,width:0,height:0,status:''}
 	  var bw = 0;
@@ -321,11 +263,11 @@ function parseUri(u) {
     return u;
   }
 
-function getNGINQueryString(id, qs, atf, add_all_tokens) {
+function getISLabQueryString(id, qs, atf, add_all_tokens) {
 	  
 	  var qstring = '?atf=' + atf;
 	  
-	  var viewport = getNGINViewport();
+	  var viewport = getISLabViewport();
 	  
 	  if(viewport && viewport.status == "ok") {
 		  qstring += '&scres_height=' + viewport.height;
@@ -358,8 +300,8 @@ function getNGINQueryString(id, qs, atf, add_all_tokens) {
 			  || key === 'zoneid' 
 			  || key === 'pzoneid' 
 			  || key === 'n' 
-			  || key === 'NGIN_domain'
-			  || key.match(/^NGIN_/)
+			  || key === 'ISLab_domain'
+			  || key.match(/^ISLab_/)
 			) { 
 			  qstring += '&' + e(key) + "=" + e(value);
 		  }
@@ -409,9 +351,9 @@ var qs = null;
 var scriptTag;
 var fpTag;
 
-if (NGIN_AdsiFrame_Opts!==null) {
+if (ISLab_AdsiFrame_Opts!==null) {
 	  
-	  qs = NGIN_AdsiFrame_Opts;
+	  qs = ISLab_AdsiFrame_Opts;
 
 } else {
 	
@@ -419,12 +361,12 @@ if (NGIN_AdsiFrame_Opts!==null) {
 
 }
 
-var cdpnLocTag = "<script type='text/javascript'>var NGIN_Loc={};" + "NGIN_Loc.loc='" + getSiteURL() 
-					+ "';NGIN_Loc.ref='" + getRefSiteURL() 
-					+ "';NGIN_Loc.ifr='" + (isInIframe() ? '1' : '0') 
-					+ "';NGIN_Loc.od='" + getOD() + "';</script>";
+var cdpnLocTag = "<script type='text/javascript'>var ISLab_Loc={};" + "ISLab_Loc.loc='" + getSiteURL() 
+					+ "';ISLab_Loc.ref='" + getRefSiteURL() 
+					+ "';ISLab_Loc.ifr='" + (isInIframe() ? '1' : '0') 
+					+ "';ISLab_Loc.od='" + getOD() + "';</script>";
 
-var domain = getQueryStringArg(qs,'NGIN_domain', adserver_domain);
+var domain = getQueryStringArg(qs,'ISLab_domain', adserverDomain);
 
 var abf = 0;
 var id = getQueryStringArg(qs, 'pzoneid');
@@ -432,7 +374,7 @@ if (!id) {
 	id = getQueryStringArg(qs, 'zoneid');
 }
 
-abf = getNGINAtf(id, getNGINViewport());
+abf = getNGINAtf(id, getISLabViewport());
 
 var org_tld = getQueryStringArg(qs, 'tld', "");
 var ct_url = getQueryStringArg(qs, 'ct0', "");
@@ -441,7 +383,7 @@ var sndprc = getQueryStringArg(qs, 'sndprc', "");
 var ui = getQueryStringArg(qs, 'ui', "");
 var cb = Math.round(new Date().getTime() / 1000);
 
-var adQueryString = getNGINQueryString(id, qs, abf, false);
+var adQueryString = getISLabQueryString(id, qs, abf, false);
 adQueryString += "&dt=in";
 adQueryString += "&buyerid=" + escape(buyer_id);
 adQueryString += "&loc=" + escape(getSiteURL());
@@ -463,9 +405,9 @@ if (isInIframe()) {
 	document.write(fpTag);
 } else {
 	  
-  if (NGIN_AdsiFrame_Opts !== null) {
+  if (ISLab_AdsiFrame_Opts !== null) {
   	
-  	var placement = NGIN_placement_id || "NGIN_FPI_" + getQueryStringArg(qs, 'z', 0);
+  	var placement = ISLab_placement_id || "ISLab_FPI_" + getQueryStringArg(qs, 'z', 0);
   	scriptTag = document.getElementById(placement) || getScriptTag();
   
   } else {
@@ -480,9 +422,9 @@ if (isInIframe()) {
  
   fpTag = '<scr'+'ipt type="text/javascript" src="http://' + domain + delivery_path + adQueryString + '"></scr'+'ipt>';
 
-  if(getQueryStringArg(qs, 'NGIN_src', '0') === '1') {
+  if(getQueryStringArg(qs, 'ISLab_src', '0') === '1') {
   	
-    var ad_server_domain = getQueryStringArg(qs, 'NGIN_ad_domain', adserver_domain);
+    var ad_server_domain = getQueryStringArg(qs, 'ISLab_ad_domain', adserverDomain);
     ifrm.src = 'http://' + ad_server_domain + delivery_path + adQueryString;
     
   } else {
@@ -496,5 +438,5 @@ if (isInIframe()) {
   return {};
 })();
 
-NGIN_placement_id = null;
-NGIN_AdsiFrame_Opts = null;
+ISLab_placement_id = null;
+ISLab_AdsiFrame_Opts = null;
--- config/autoload/delivery.local.php
+++ config/autoload/delivery.local.php
@@ -1,21 +1,13 @@
 <?php
-/**
- * CDNPAL NGINAD Project
- *
- * @link http://www.nginad.com
- * @copyright Copyright (c) 2013-2015 CDNPAL Ltd. All Rights Reserved
- * @license GPLv3
- */
 
 /**
  * Delivery Configuration Override
  */
 
 return array(
-
 		'delivery' => array(
 				'url' => "http://localhost/delivery/impress",
-				'adtag' => "http://localhost/ad/nginad.js",
+				'adtag' => "http://localhost/bgate.js",
 				'in_house_ad_grep' => "localhost/delivery",
                 'cache_file_location' => "cache/delivery/"
 		),
