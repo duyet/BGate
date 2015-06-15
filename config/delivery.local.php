e63c0499e4dbec64ca1bc6a5c4193e01d8a03fe2
Trong Thao <trongthaonh@gmail.com>
2015-06-15 10:28:25 +0700

    Fix basePath in delivery.local

--- config/delivery.local.php
+++ config/delivery.local.php
@@ -3,12 +3,28 @@
 /**
  * Delivery Configuration Override
  */
+function getBaseUrl() {
+	// output: /myproject/index.php
+	$currentPath = $_SERVER['PHP_SELF'];
 
+	// output: Array ( [dirname] => /myproject [basename] => index.php [extension] => php [filename] => index )
+	$pathInfo = pathinfo($currentPath);
+
+	// output: localhost
+	$hostName = $_SERVER['HTTP_HOST'];
+
+	// output: http://
+	$protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https://'?'https://':'http://';
+
+	// return: http://localhost/myproject/
+	return $protocol.$hostName.$pathInfo['dirname']."/";
+}
+$request_path =  getBaseUrl();
 return array(
 		'delivery' => array(
-				'url' => "http://localhost/delivery/impress",
-				'adtag' => "http://localhost/bgate/bgate.js",
-				'in_house_ad_grep' => "localhost/delivery",
+				'url' => $request_path."delivery/impress",
+				'adtag' => $request_path."bgate.js",
+				'in_house_ad_grep' => $request_path."delivery",
                 'cache_file_location' => "cache/delivery/"
 		),
 );
