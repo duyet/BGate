5bed38c6cffadf2b19152c72e28ed3c4f959fbd3
Trung Le <trungle.cs@gmail.com>
2015-06-19 10:12:37 +0700

    update bgate.js

--- bgate.js
+++ bgate.js
@@ -6,7 +6,6 @@
  */
 
 var adx_url = "http://ptnhttt.uit.edu.vn:12339/bids";
-var ip_getter = 'http://jsonip.appspot.com';
 var adserverDomain = 'localhost';
 var delivery_path = '/delivery/impress';
 
@@ -435,6 +434,56 @@ var InitLvDuitIFrame = (function () {
 
     var adQueryString = getISLabQueryString(id, qs, abf, false);
 
+    // adQueryString += "&dt=in";
+    // adQueryString += "&buyerid=" + escape(buyer_id);
+    // adQueryString += "&loc=" + escape(getSiteURL());
+    // adQueryString += "&ref=" + escape(getRefSiteURL());
+    // adQueryString += "&ifr=" + (isInIframe() ? '1' : '0');
+    // adQueryString += "&tld=" + escape(getOD());
+    // adQueryString += "&sndprc=" + escape(sndprc);
+    // adQueryString += "&ui=" + ui;
+    // adQueryString += "&ct=" + escape(ct_url);
+    // adQueryString += "&org_tld=" + escape(org_tld);
+    // adQueryString += "&cb=" + cb;
+
+    // var fpTag = '<scr'+'ipt type="text/javascript" src="http://' + domain + delivery_path
+    // 			  + adQueryString + '"></scr' + 'ipt>';
+    // var htmlPrefix = "<html><head><title></title></head><body style='padding:0px;margin:0px;'>";
+    // var htmlSuffix = "<![if !IE]><script type='text/javascript'>document.close();</script><![endif]></body></html>";
+
+    // if (isInIframe()) {
+    // 	document.write(fpTag);
+    // } else {
+	  
+    //   if (ISLab_AdsiFrame_Opts !== null) {
+  	
+    //   	var placement = ISLab_placement_id || "ISLab_FPI_" + getQueryStringArg(qs, 'z', 0);
+    //   	scriptTag = document.getElementById(placement) || getScriptTag();
+  
+    //   } else {
+    //   	scriptTag = getScriptTag();
+    //   }
+  
+    //   var width = getQueryStringArg(qs, 'width', 160);
+    //   var height = getQueryStringArg(qs, 'height', 600);
+    //   var ifrm = createiFrame(id, width, height);
+
+    //   scriptTag.parentNode.insertBefore(ifrm, scriptTag);
+ 
+    //   fpTag = '<scr'+'ipt type="text/javascript" src="http://' + domain + delivery_path + adQueryString + '"></scr'+'ipt>';
+
+    //   if(getQueryStringArg(qs, 'ISLab_src', '0') === '1') {
+  	
+    //     var ad_server_domain = getQueryStringArg(qs, 'ISLab_ad_domain', adserverDomain);
+    //     ifrm.src = 'http://' + ad_server_domain + delivery_path + adQueryString;
+    
+    //   } else {
+  	
+    //     var ifr_content = ifrm.contentWindow.document || ifrm.contentDocument;
+    //     ifr_content.write(htmlPrefix + fpTag + htmlSuffix);
+    
+    //   }
+    // }
 
     return {};
 })();
