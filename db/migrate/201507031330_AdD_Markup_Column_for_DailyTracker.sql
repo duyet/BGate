01a05453d2d63a245b1505e0dea8194ef69993ab
Duyệt <lvduit08@gmail.com>
2015-07-03 14:20:38 +0700

    Update 201507031330_AdD_Markup_Column_for_DailyTracker.sql

--- db/migrate/201507031330_AdD_Markup_Column_for_DailyTracker.sql
+++ db/migrate/201507031330_AdD_Markup_Column_for_DailyTracker.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `AdBannerDailyTracker` 
-ADD COLUMN `MarkupOutcome` FLOAT NULL DEFAULT 0 AFTER `Outcome`;
+ADD COLUMN `NetOutcome` FLOAT NULL DEFAULT 0 AFTER `Outcome`;
 
 ALTER TABLE `AdzoneDailyTracker` 
-ADD COLUMN `MarkupIncome` FLOAT NULL DEFAULT 0 AFTER `Income`;
+ADD COLUMN `NetIncome` FLOAT NULL DEFAULT 0 AFTER `Income`;
 
